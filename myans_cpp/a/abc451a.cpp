eafd534fd59bc7cef51af46ff6179eecc61ad9db
smitsu32 <33113176@stn.nitech.ac.jp>
2026-03-29 00:26:04 +0900

    abc451a.cppを新規作成

---
+++ myans_cpp/a/abc451a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define reps(i,s,n) for(int i=(s);i<(s)+(int)(n);i++)
+
+int main() {
+    string s;
+    cin >> s;
+    if (size(s)%5==0){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
